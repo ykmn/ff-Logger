aae9726ba808aded6a4b1572dd781e4c34fd5c1e
ykmn <livcey@gmail.com>
2024-09-10 17:15:13 +0300

    v1.02 Move config to external file
    Параметры каналов записи вынесены в файл конфигурации; добавлен прогресс-бар времени перезапуска логгеров.

--- ff-Logger.ps1
+++ ff-Logger.ps1
@@ -16,18 +16,19 @@ function Start-ffmpeg {
     if (!(Test-Path $storage\$name\$tomorrow)) { New-Item -Path $storage\$name\$tomorrow -Force -ItemType Directory | Out-Null }
     $output = $storage+"\"+$name+"\$today"+"\%H-%M-%S."+$ext
 
-    if ($url.Contains("@device")) { $parameters = " -f dshow -i audio=$url " }
-    if ($url.Contains("http"))    { $parameters = " -i $url -c:a copy " }
-    if ($_.bitrate)               { $parameters = $parameters + " -ar 44100 -ac 2 -ab " + $bitrate }
+    if ($url.Contains("@device")) { $parameters = " -f dshow -i audio=$url" }
+    if ($url.Contains("http"))    { $parameters = " -i $url" }
+    if ($bitrate)                 { $parameters = $parameters + " -ar 44100 -ac 2 -ab " + $bitrate }
+                             else { $parameters = $parameters + " -c:a copy" }
     
     $parameters = $parameters + " -f segment -segment_time 00:10:00 -strftime 1 -strftime_mkdir 1 -segment_atclocktime 1 -reset_timestamps 1 -v warning -stats -loglevel warning -y $output"
     # Log levels are:
     # quiet: -8, panic: 0, fatal: 8, error: 16, warning: 24, info: 32, verbose: 40, debug: 48, trace: 56
     
-    #Write-Host "Parameters for $name :" $parameters -ForegroundColor DarkGray
-    $parameters = $parameters.Split(" ") # lol
+    #Write-Host "ffmpeg parameters for $name :" $parameters -ForegroundColor DarkGray
+    $parameters = $parameters.Split(" ") # lol because of Start-Process ArgumentList
     Write-Host (Get-Date -Format "yyyy/MM/dd HH:mm:ss") "Starting" $name "Logger" -ForegroundColor DarkGreen
-    $proc = Start-Process -filePath ffmpeg.exe -ArgumentList $parameters -Passthru -WindowStyle Normal
+    $proc = Start-Process -filePath ffmpeg.exe -ArgumentList $parameters -Passthru -WindowStyle Minimized
     
     # Wait for a quirk: adding window title
     Add-Type -Type @"
@@ -59,7 +60,7 @@ function Stop-ffmpeg {
 
 ####################################################################################################################################
 Clear-Host
-Write-Host "`nff-Logger.ps1 Version 1.01.001 <r.ermakov@emg.fm> 2024-09-09 https://github.com/ykmn/ff-Logger `n"
+Write-Host "`nff-Logger.ps1 Version 1.02.001 <r.ermakov@emg.fm> 2024-09-10 https://github.com/ykmn/ff-Logger `n"
 # Check for Powershell Core
 if ($PSVersionTable.PSEdition -eq "Core") {
     Write-Host "PowerShell Core detected, OK " -ForegroundColor DarkGreen -NoNewline
@@ -76,15 +77,15 @@ if ($PSVersionTable.PSEdition -eq "Core") {
 # Defining stations
 # If you need to list audio devices, use
 # ffmpeg -list_devices true -f dshow -i dummy
-$stations = @(
-#    @{ name = 'Audio Input 1';     url = '@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{F84408DF-9C57-4C98-A66F-FBCC9EA194DD}'; ext = 'mp3'; storage = 'D:\STORAGE';
-#                                   bitrate = '64K'},
-    @{ name = 'Megapolis';         url = 'https://megapolisfm.hostingradio.ru/megapolisfm96.aacp';   ext = 'aac'; storage = 'D:\STORAGE' },
-    @{ name = 'Autoradio Moscow';  url = 'https://pub0202.101.ru:8000/stream/air/aac/64/100';        ext = 'aac'; storage = 'D:\STORAGE' },
-    @{ name = 'Radio JAZZ';        url = 'https://nashe1.hostingradio.ru:80/jazz-128.mp3';           ext = 'mp3'; storage = 'D:\STORAGE' },
-    @{ name = 'Monte Carlo';       url = 'https://montecarlo.hostingradio.ru/montecarlo96.aacp';     ext = 'aac'; storage = 'D:\STORAGE' }
-
-);
+$currentdir = Split-Path $MyInvocation.MyCommand.Path -Parent
+if (!(Test-Path $currentdir\stations.json)) {
+    Write-Host "Configuration file not found." -ForegroundColor Red
+    Break
+}
+# Reading JSON
+$stations = Get-Content -Raw -Path $currentdir\stations.json | ConvertFrom-Json
+Write-Host "Use the following configuration file:" -ForegroundColor DarkGreen
+$stations
 
 # Initial loggers start
 $stations | ForEach-Object {
@@ -95,43 +96,51 @@ $stations | ForEach-Object {
     }
 }
 
-$triggerTime = "00:00:00am"
-Write-Host "There is" ((Get-Date -Date $triggerTime).AddDays(1)-(Get-Date)) "before scheduled loggers re-launch.`n" -ForegroundColor DarkGray
-
+$triggerTime = "00:00:01am"
 # Infinite loop   
 do {
-    if ((Get-Date) -lt (Get-Date -Date $triggerTime)) {
-    # Before trigger time: sleeping for the remaining time
-        while ((Get-Date) -lt (Get-Date -Date $triggerTime)) {
-            Write-Host "It is" ((Get-Date -Date $triggerTime).AddDays(1)-(Get-Date)) "before scheduled loggers re-launch.`n" -ForegroundColor DarkGray
-            (Get-Date -Date $triggerTime)-(Get-Date) | Start-Sleep
-        }
-        # Trigger here!
-        Write-Host (Get-Date -Format "yyyy/MM/dd HH:mm:ss") "It's trigger time, restarting loggers:" -ForegroundColor Yellow
+    if ((Get-Date -Format "HH:mm:ss") -eq (Get-Date -Date $triggerTime -Format "HH:mm:ss")) {
+        # It's trigger time!
+        Write-Host (Get-Date -Format "yyyy/MM/dd HH:mm:ss") "It's a trigger time, restarting loggers" -ForegroundColor Yellow
         $stations | ForEach-Object {
             Stop-ffmpeg $_.name
-            if ($_.format) {
-                Start-ffmpeg $_.url $_.name $_.ext $_.storage $_.format    
+        }
+    }
+
+    # It's not trigger time
+    if ($((Get-Date)-(Get-Date -Date $triggerTime)) -lt 0) {
+        # Trigger time is in future
+        $progress = (Get-Date -Date $triggerTime)-(Get-Date)
+    } else {
+        # Trigger time was in past, so set trigger to next day
+        $progress = (Get-Date -Date $triggerTime).AddDays(1)-(Get-Date)
+    }
+    # Show progress
+    $percent = [math]::Round((100 - ($progress.TotalSeconds * 100 / 86400)),2)
+    Write-Progress -Activity "Time left until loggers restart: " -Status $('{0:hh\:mm\:ss}' -f $progress) -PercentComplete $percent
+
+    # Check for running process
+    $stations | ForEach-Object {
+        $n = $_.name.Replace(" ","-")
+        $id = (Get-Process | Where-Object {$_.MainWindowTitle -eq "$n Logger"}).Id
+        if (!($id)) {
+            Write-Host (Get-Date -Format "yyyy/MM/dd HH:mm:ss") $_.name "Logger process not found" -ForegroundColor DarkRed
+            if ($_.bitrate) {
+                Start-ffmpeg $_.url $_.name $_.ext $_.storage $_.bitrate    
             } else {
                 Start-ffmpeg $_.url $_.name $_.ext $_.storage
             }
         }
-        Write-Host "There is" ((Get-Date -Date $triggerTime).AddDays(1)-(Get-Date)) "before scheduled loggers re-launch.`n" -ForegroundColor DarkGray
-    } else {
-    #  After trigger time: check for running process"
-        $stations | ForEach-Object {
-            $n = $_.name.Replace(" ","-")
-            $id = (Get-Process | Where-Object {$_.MainWindowTitle -eq "$n Logger"}).Id
-            if (!($id)) {
-                Write-Host (Get-Date -Format "yyyy/MM/dd HH:mm:ss") $_.name "Logger process not found" -ForegroundColor DarkRed
-                if ($_.bitrate) {
-                    Start-ffmpeg $_.url $_.name $_.ext $_.storage $_.bitrate    
-                } else {
-                    Start-ffmpeg $_.url $_.name $_.ext $_.storage
-                }
-                Write-Host "There is" ((Get-Date -Date $triggerTime).AddDays(1)-(Get-Date)) "before scheduled loggers re-launch.`n" -ForegroundColor DarkGray
-            }
-        }
     }
+
 } while ($true)
+
 exit
+
+<#
+Versions:
+
+* 2024-09-06 - v1.00 Начальная версия.
+* 2024-09-09 - v1.01 Добавлена возможность записи с аудиовхода звуковой карты, что уж там.
+* 2024-09-10 - v1.02 Параметры каналов записи вынесены в файл конфигурации; добавлен прогресс-бар времени перезапуска логгеров.
+#>
